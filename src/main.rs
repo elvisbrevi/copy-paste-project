cd8f28c6e4ffd225a8c01b05bdb9abb423d7a269
elvisbrevi <elvisbrevi@gmail.com>
2023-11-08 08:10:49 -0300

    done basic functionality, copy files, directories or text

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,61 @@
-// extern crate clipboard;
-
-// use clipboard::ClipboardContext;
-// use clipboard::ClipboardProvider;
-// use std::{thread::sleep, time::Duration};
-
-// fn main() {
-//     let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
-//     let mut last_contents = String::new();
-
-//     loop {
-//         match ctx.get_contents() {
-//             Ok(contents) => {
-//                 if contents != last_contents {
-//                     println!("Nuevo contenido detectado en el portapapeles! {}", contents);
-//                     last_contents = contents.clone();
-//                     // Intenta interpretar el contenido como una ruta de archivo
-//                     if let Ok(metadata) = std::fs::metadata(&contents) {
-//                         if metadata.is_file() {
-//                             println!("El contenido parece ser una ruta de archivo: {}", contents);
-//                         } else if metadata.is_dir() {
-//                             println!(
-//                                 "El contenido parece ser una ruta de directorio: {}",
-//                                 contents
-//                             );
-//                         }
-//                     } else {
-//                         println!("solo texto");
-//                     }
-//                 }
-//             }
-//             Err(e) => println!("Error al acceder al portapapeles: {:?}", e),
-//         }
-
-//         sleep(Duration::from_secs(2));
-//     }
-// }
-
+use clipboard::ClipboardContext;
+use clipboard::ClipboardProvider;
 use clipboard_files;
 
+#[derive(PartialEq)]
+enum ClipboardType {
+    FilesOrDirectories,
+    Text,
+}
+
 fn main() {
-    let mut last_contents = String::from("");
+    let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
+    let mut old_contents = String::from("");
+    let mut new_contents = String::new();
+    let mut clipboard_type;
 
     loop {
+        // if clipboard is file or directory
         match clipboard_files::read() {
             Ok(file_paths) => {
-                let mut new_contents = String::new();
+                new_contents = String::new();
+                clipboard_type = ClipboardType::FilesOrDirectories;
                 // reading clipboard
                 for file_path in file_paths {
                     match file_path.to_str() {
                         Some(value) => {
-                            if file_path.is_file() {
-                                //println!("Files: {}", value);
-                            }
-                            if file_path.is_dir() {
-                                //println!("Dir: {}", value);
-                            }
-
                             new_contents += value;
                         }
-                        None => {
-                            new_contents = "nada".to_string();
-                        }
+                        None => {}
                     }
                 }
-
-                if last_contents != new_contents {
-                    println!("old: {} \nnew: {}", last_contents, new_contents);
-                    println!("---------------------------------------------");
-                }
-                last_contents = new_contents.clone();
             }
-            Err(e) => {
-                //new_contents = "error".to_string();
+            Err(_) => {
+                clipboard_type = ClipboardType::Text;
             }
         }
+
+        // if clipboard is only text
+        if clipboard_type == ClipboardType::Text {
+            match ctx.get_contents() {
+                Ok(contents) => {
+                    if contents != old_contents {
+                        new_contents = contents.clone();
+                    }
+                }
+                Err(_) => {}
+            }
+        }
+
+        // action with clipboard
+        if old_contents != new_contents {
+            match clipboard_type {
+                ClipboardType::FilesOrDirectories => println!("archivo o directorio:"),
+                ClipboardType::Text => println!("texto:"),
+            }
+            println!("old: {} \nnew: {}", old_contents, new_contents);
+            println!("---------------------------------------------");
+        }
+        old_contents = new_contents.clone();
     }
 }
